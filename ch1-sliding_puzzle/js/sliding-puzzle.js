785e03ff588154603108cba8ad8989487d7c5675
Adam Hall <afraidofheights.net@gmail.com>
2015-08-24 11:19:04 -0400

    Finished chapter 1

--- ch1-sliding_puzzle/js/sliding-puzzle.js
+++ ch1-sliding_puzzle/js/sliding-puzzle.js
@@ -1,27 +1,27 @@
 $(function() {
     var numberOfPieces = 12,
-    aspect = "3:4",
-    aspectW = parseInt(aspect.split(":")[0]),
-    aspectH = parseInt(aspect.split(":")[1]),
-    container = $("#puzzle"),
-    imgContainer = container.find("figure"),
-    img = imgContainer.find("img"),
-    path = img.attr("src"),
-    piece = $("<div/>"),
-    pieceW = Math.floor(img.width() / aspectW),
-    pieceH = Math.floor(img.height() / aspectH),
-    idCounter = 0,
-    positions = [],
-    empty = {
-        top: 0, 
-        left: 0,
-        bottom: pieceH, 
-        right: pieceW
-    },
-    previous = {},
-    timer,
-    currentTime = {},
-    timerDisplay = container.find("#time").find("span");
+        aspect = "3:4",
+        aspectW = parseInt(aspect.split(":")[0]),
+        aspectH = parseInt(aspect.split(":")[1]),
+        container = $("#puzzle"),
+        imgContainer = container.find("figure"),
+        img = imgContainer.find("img"),
+        path = img.attr("src"),
+        piece = $("<div/>"),
+        pieceW = Math.floor(img.width() / aspectW),
+        pieceH = Math.floor(img.height() / aspectH),
+        idCounter = 0,
+        positions = [],
+        empty = {
+            top: 0, 
+            left: 0,
+            bottom: pieceH, 
+            right: pieceW
+        },
+        previous = { },
+        timer,
+        currentTime = { },
+        timerDisplay = container.find("#time").find("span");
     
     for(var x = 0, y = aspectH; x < y; x++) {
         for(var a = 0, b = aspectW; a < b; a++) {
@@ -42,18 +42,18 @@ $(function() {
                          "-", top, "px"
                      ].join("")
             }).appendTo(imgContainer);
-                
+
             positions.push({ top: top, left: left });
         }
     }
-    
+
     img.remove();
     container.find("#0").remove();
     positions.shift();
-    
+
     $("#start").on("click", function(e) {
         var pieces = imgContainer.children();
-        
+
         function shuffle(array) {
             var i = array.length;
             
@@ -82,5 +82,94 @@ $(function() {
         empty.left = 0;
         
         container.find("#ui").find("p").not("#time").remove();
+    
+        pieces.appendTo(imgContainer).draggable("destroy");
+
+        if(timer) {
+            clearInterval(timer);
+            timerDisplay.text("00:00:00");
+        }
+
+        timer = setInterval(updateTime, 1000);
+        currentTime.seconds = 0;
+        currentTime.minutes = 0;
+        currentTime.hours = 0;
+
+        function updateTime() {
+            if(currentTime.hours === 23 && 
+                   currentTime.minutes === 59 && 
+                   currentTime.seconds === 59) {
+                clearInterval(timer);
+            }
+        }
+    
+        pieces.draggable({
+            containment: "parent",
+            grid: [pieceW, pieceH],
+            start: function(e, ui) {
+                var current = getPosition(ui.helper);
+                
+                if(current.left === empty.left) {
+                    ui.helper.draggable("option", "axis", "y");
+                } else if(current.top === empty.top) {
+                    ui.helper.draggable("option", "axis", "x");
+                } else {
+                    ui.helper.trigger("mouseup");
+                    return false;
+                }
+                
+                if(current.bottom < empty.top ||
+                   current.top > empty.bottom ||
+                   current.left > empty.right ||
+                   current.right < empty.left) {
+                       ui.helper.trigger("mouseup");
+                       return false;                    
+                   }
+                   
+                   previous.top = current.top;
+                   previous.left = current.left;
+            },
+            drag: function(e, ui) {
+                var current = getPosition(ui.helper);
+                
+                ui.helper.draggable("option", "revert", false);
+                
+                if(current.top === empty.top && current.left === empty.left) {
+                    ui.helper.trigger("mouseup");
+                    return false;
+                }
+                
+                if(current.top > empty.bottom ||
+                   current.bottom < empty.top ||
+                   current.left > empty.right ||
+                   current.right < empty.left) {
+                       ui.helper.trigger("mouseup")
+                                .css({
+                                    top: previous.top,
+                                    left: previous.left
+                                });
+                       return false;
+                   }
+            },
+            stop: function(e, ui) {
+                var current = getPosition(ui.helper);
+                
+                if(current.top === empty.top && current.left === empty.left) {
+                    empty.top = previous.top;
+                    empty.left = previous.left;
+                    empty.bottom = previous.top + pieceH;
+                    empty.right = previous.left + pieceW;
+                }
+            }
+        });
+        
+        function getPosition(el) {
+            return {
+                top: parseInt(el.css("top")),
+                bottom: parseInt(el.css("top")) + pieceH,
+                left: parseInt(el.css("left")),
+                right: parseInt(el.css("left")) + pieceW
+            }
+        }
     });
 });
